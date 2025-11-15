521cd81ced6d7cf49a71081785a2070ca4803b80
Octavian <mihai.g.octavian24@stud.rau.ro>
2025-11-15 23:48:32 +0200

    quick fix pe route

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@/lib/supabase/server'
 import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
@@ -8,8 +7,7 @@ export async function GET(request: Request) {
   const next = requestUrl.searchParams.get('next') || '/'
 
   if (code) {
-    const cookieStore = cookies()
-    const supabase = await createClient(cookieStore)
+    const supabase = await createClient()
 
     const { error } = await supabase.auth.exchangeCodeForSession(code)
 
